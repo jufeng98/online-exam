70d609efc1d989dbb6c720a0bb6225b82fe1b997
梁煜东 <375709770@qq.com>
2019-08-05 17:32:33 +0800

    初始化碎片学习安卓项目

---
+++ fragmentlearning/app/src/main/java/org/javamaster/fragmentlearning/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package org.javamaster.fragmentlearning.ui.login
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import org.javamaster.fragmentlearning.data.LoginDataSource
+import org.javamaster.fragmentlearning.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
