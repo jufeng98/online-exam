eb8cd806925d3e5a47d4ff765793a693b0cecbd3
jufeng98 <375709770@qq.com>
2020-04-17 18:45:48 +0800

    完善mybatis-generator

--- b2c-parent/b2c-core/src/main/java/org/javamaster/b2c/core/enums/TopicsTypeEnum.java
+++ b2c-parent/b2c-core/src/main/java/org/javamaster/b2c/core/enums/TopicsTypeEnum.java
@@ -4,23 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @author yudong
- * @date 2019/6/10
+ * 请勿手工改动此文件,请使用 mybatis generator
+ *
+ * @author mybatis generator
  */
-public enum TopicsTypeEnum implements EnumBase {
-    UNKNOWN(0, ""),
-    CODING(1, "编码挑战"),
+public enum TopicsTypeEnum {
+    /**
+     * 未知枚举值
+     */
+    UNKNOWN(-1, ""),
+    /**
+     * 编程挑战
+     */
+    CODING(1, "编程挑战"),
+    /**
+     * 网页开发
+     */
     WEB(2, "网页开发"),
+    /**
+     * 编程语言
+     */
     LANG(3, "编程语言"),
+    /**
+     * 数据科学
+     */
     DATA(4, "数据科学"),
-    DEV(5, "开发基础"),
+    /**
+     * 开发基础
+     */
+    FUND(5, "开发基础"),
     ;
     private final int code;
     private final String msg;
     private static final Map<Integer, TopicsTypeEnum> MAP;
 
     static {
-        MAP = new HashMap<>(6, 1);
+        MAP = new HashMap<>(8, 1);
         for (TopicsTypeEnum value : TopicsTypeEnum.values()) {
             MAP.put(value.getCode(), value);
         }
@@ -31,16 +50,14 @@ public enum TopicsTypeEnum implements EnumBase {
         this.msg = msg;
     }
 
-    public static TopicsTypeEnum getEnumByCode(Integer code) {
+    public static TopicsTypeEnum getByCode(Integer code) {
         return MAP.getOrDefault(code, UNKNOWN);
     }
 
-    @Override
     public int getCode() {
         return code;
     }
 
-    @Override
     public String getMsg() {
         return msg;
     }
